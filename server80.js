79392507cd69d28604fcf0dafba8416701410f5d
devalvarado200 <35086743+devalvarado200@users.noreply.github.com>
2021-08-25 09:31:34 -0500

    Update server80.js

--- server80.js
+++ server80.js
@@ -1,13 +1,16 @@
 const http = require("http");
+const ip = require("ip");
 
+const ip_address = ip.address();
 const port = 80;
 
 const server = http.createServer((req, res)=>{
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/plain");
-    res.end("Sample Web Server, Port: 80");
+    res.end("Sample Web Server, Port: 3000");
 })
 
 server.listen(port, () => {
+    console.log (`My IP is: ${ip_address}`);
     console.log(`Server running on port: ${port}`);
 })
